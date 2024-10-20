fffd799aeb5078c005dc0b4fe4ca77c901067c65
roma1st <roman.stroziuk@oa.edu.ua>
2024-10-20 18:31:07 +0200

    feat : dtos
    done.

---
+++ Api/Dtos/BrandDtos.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Brands;
+
+namespace Api.Dtos;
+
+public record BrandDto(Guid? Id, string Name)
+{
+    public static BrandDto FromDomainModel(Brand brand)
+        => new(brand.Id.Value, brand.Name);
+}
---
+++ Api/Dtos/CategoryDtos.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Сategories;
+
+namespace Api.Dtos;
+
+public record CategoryDto(Guid? Id, string Name)
+{
+    public static CategoryDto FromDomainModel(Category category)
+        => new(category.Id.Value, category.Name);
+}
