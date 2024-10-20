1f5e86f05f0dfa4631d0fd2ba9d50e74fd1355d0
roma1st <roman.stroziuk@oa.edu.ua>
2024-10-20 18:16:59 +0200

    feat : errorhandler
    done

--- Api/Modules/Errors/SneakerErrorHandler.cs
+++ Api/Modules/Errors/SneakerErrorHandler.cs
@@ -1,23 +1,24 @@
-﻿// //using Application.Sneakers.Exceptions;
-// using Microsoft.AspNetCore.Mvc;
-//
-// namespace Api.Modules.Errors;
-//
-// public static class SneakerErrorHandler
-// {
-//     public static ObjectResult ToObjectResult(this SneakerException exception)
-//     {
-//         return new ObjectResult(exception.Message)
-//         {
-//             StatusCode = exception switch
-//             {
-//                 SneakerNotFoundException or
-//                     SneakerBrandNotFoundException or
-//                          SneakerCategoryNotFoundException => StatusCodes.Status404NotFound,
-//                 SneakerAlreadyExistsException => StatusCodes.Status409Conflict,
-//                 SneakerUnknownException => StatusCodes.Status500InternalServerError,
-//                 _ => throw new NotImplementedException("Sneaker error handler does not implemented")
-//             }
-//         };
-//     }
-// .
+﻿using Microsoft.AspNetCore.Mvc;
+using Application.Sneakers.Exceptions;
+
+
+namespace Api.Modules.Errors;
+
+public static class SneakerErrorHandler
+{
+    public static ObjectResult ToObjectResult(this SneakerException exception)
+    {
+        return new ObjectResult(exception.Message)
+        {
+            StatusCode = exception switch
+            {
+                SneakerNotFoundException or
+                    SneakerBrandNotFoundException or
+                    SneakerCategoryNotFoundException => StatusCodes.Status404NotFound,
+                SneakerAlreadyExistsException => StatusCodes.Status409Conflict,
+                SneakerUnknownException => StatusCodes.Status500InternalServerError,
+                _ => throw new NotImplementedException("Sneaker error handler does not implemented")
+            }
+        };
+    }
+}
