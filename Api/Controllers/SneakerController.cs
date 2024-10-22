47dc061f969f026e964e410afc1fa40261b47e77
roma1st <roman.stroziuk@oa.edu.ua>
2024-10-22 19:30:15 +0200

    feat : Controllers to S,C,B
    all done.

---
+++ Api/Controllers/SneakerController.cs
@@ -0,0 +1,85 @@
+﻿using Api.Dtos;
+using Api.Modules.Errors;
+using Application.Common.Interfaces.Queries;
+using Application.Sneakers.Commands;
+using Domain.Sneakers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[Route("sneakers")]
+[ApiController]
+public class SneakerController(ISender sender, ISneakerQueries sneakerQueries) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<SneakerDto>>> GetAll(CancellationToken cancellationToken)
+    {
+        var entities = await sneakerQueries.GetAll(cancellationToken);
+
+        return entities.Select(SneakerDto.FromDomainModel).ToList();
+    }
+
+    [HttpGet("{sneakerId:guid}")]
+    public async Task<ActionResult<SneakerDto>> Get([FromRoute] Guid sneakerId, CancellationToken cancellationToken)
+    {
+        var entity = await sneakerQueries.GetById(new SneakerId(sneakerId), cancellationToken);
+
+        return entity.Match<ActionResult<SneakerDto>>(
+            s => SneakerDto.FromDomainModel(s),
+            () => NotFound());
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<SneakerDto>> Create([FromBody] SneakerDto request, CancellationToken cancellationToken)
+    {
+        var input = new CreateSneakerCommand
+        {
+            Model = request.model,
+            Size = request.size,
+            Price = request.price,
+            CategoryId = request.CategoryId,
+            BrandId = request.BrandId
+            
+        };
+
+        var result = await sender.Send(input, cancellationToken);
+
+        return result.Match<ActionResult<SneakerDto>>(
+            s => SneakerDto.FromDomainModel(s),
+            e => e.ToObjectResult());
+    }
+
+    [HttpPut("{sneakerId:guid}")]
+    public async Task<ActionResult<SneakerDto>> Update([FromBody] SneakerDto request, CancellationToken cancellationToken)
+    {
+        var input = new UpdateSneakerCommand
+        {
+            SneakerId = request.Id!.Value,
+            Model = request.model,
+            Size = request.size,
+            Price = request.price
+        };
+
+        var result = await sender.Send(input, cancellationToken);
+
+        return result.Match<ActionResult<SneakerDto>>(
+            sneaker => SneakerDto.FromDomainModel(sneaker),
+            e => e.ToObjectResult());
+    }
+
+    [HttpDelete("{sneakerId:guid}")]
+    public async Task<ActionResult<SneakerDto>> Delete([FromRoute] Guid sneakerId, CancellationToken cancellationToken)
+    {
+        var input = new DeleteSneakerCommand
+        {
+            SneakerId = sneakerId
+        };
+
+        var result = await sender.Send(input, cancellationToken);
+
+        return result.Match<ActionResult<SneakerDto>>(
+            s => SneakerDto.FromDomainModel(s),
+            e => e.ToObjectResult());
+    }
+}
---
+++ Api/Modules/Validators/SneakerDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using Api.Dtos;
+using FluentValidation;
+
+namespace Api.Modules.Validators;
+
+public class SneakerDtoValidator : AbstractValidator<SneakerDto>
+{
+    public SneakerDtoValidator()
+    {
+        RuleFor(x => x.model).NotEmpty().MaximumLength(255).MinimumLength(3);
+    }
+}
