18051dceec877d67725deceb46b18b62099b4fb5
roma1st <roman.stroziuk@oa.edu.ua>
2024-10-23 13:57:42 +0200

    fix: SneakerDtoValidator

--- Api/Modules/Validators/SneakerDtoValidator.cs
+++ Api/Modules/Validators/SneakerDtoValidator.cs
@@ -8,5 +8,9 @@ public class SneakerDtoValidator : AbstractValidator<SneakerDto>
     public SneakerDtoValidator()
     {
         RuleFor(x => x.model).NotEmpty().MaximumLength(255).MinimumLength(3);
+        
+        RuleFor(x => x.size)
+            .NotEmpty().WithMessage("Size is required.")
+            .InclusiveBetween(35, 50).WithMessage("Size must be between 35 and 50.");
     }
 }
